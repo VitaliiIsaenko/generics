ed84ec0df8793eb5c96f59217bb97fa812c53972
Vitalii Isaenko <lookyisaenko@gmail.com>
2017-12-02 17:21:48 +0100

    Add study example

---
+++ testgroupid/src/test/java/singleton/Singleton.java
@@ -0,0 +1,11 @@
+package singleton;
+
+public class Singleton<T> {
+    public static T getInstance()
+    {
+        if (instance == null)
+            instance = new Singleton<T>();
+        return instance;
+    }
+    private static T instance = null;
+} 
